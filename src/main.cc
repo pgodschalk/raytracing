ab098a6bbc6399b766ef09364bf16f4301574636
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-02 14:52:50 +0100

    feat: add logging for scanlines and completion
    - Add logging of remaining scanlines and a completion message to provide progress updates during image processing.

--- src/main.cc
+++ src/main.cc
@@ -9,6 +9,9 @@ int main() {
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
+
     for (int i = 0; i < image_width; i++) {
       double r = double(i) / (image_width - 1);
       double g = double(j) / (image_height - 1);
@@ -21,4 +24,6 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
+
+  std::clog << "\rDone.                 \n";
 }
