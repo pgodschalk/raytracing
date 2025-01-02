512ee61951b15bf47aa1e8a6e75a2b967eb1b9b9
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-02 15:51:25 +0100

    chore: add C++11 flag and use auto types
    - Add the C++11 standard flag to the compile settings.
    - Use `auto` for variable type declarations to enhance type inference.

--- src/main.cc
+++ src/main.cc
@@ -13,9 +13,9 @@ int main() {
               << std::flush;
 
     for (int i = 0; i < image_width; i++) {
-      double r = double(i) / (image_width - 1);
-      double g = double(j) / (image_height - 1);
-      double b = 0.0;
+      auto r = double(i) / (image_width - 1);
+      auto g = double(j) / (image_height - 1);
+      auto b = 0.0;
 
       int ir = int(255.999 * r);
       int ig = int(255.999 * g);
