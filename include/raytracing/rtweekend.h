5ffeb70710a97b627d4107e5cfd9eba72a808707
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-07 12:52:13 +0100

    feat: add random_double function for uniform random
    - Add a `random_double` function for generating uniform random numbers.

--- include/raytracing/rtweekend.h
+++ include/raytracing/rtweekend.h
@@ -1,8 +1,10 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 // C++ Std Usings
 
@@ -18,6 +20,12 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
 }
 
 #endif
