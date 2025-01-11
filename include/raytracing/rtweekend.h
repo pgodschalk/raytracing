5d0f138f713c4451513010bc10cdb4542e22acb8
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-11 17:45:02 +0100

    feat: add random double generator function
    - Add a function to generate a random double between a minimum and maximum value

--- include/raytracing/rtweekend.h
+++ include/raytracing/rtweekend.h
@@ -27,4 +27,9 @@ inline double random_double() {
   return distribution(generator);
 }
 
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max - min) * random_double();
+}
+
 #endif
