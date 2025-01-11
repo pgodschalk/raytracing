0bd8bf9b8aa1642ee838077f3e8ac4da6cbad30c
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-11 21:51:44 +0100

    feat: add material support to hittable and sphere
    - Enhance hit_record with material support by introducing the material class and adding a shared_ptr to it.
    - Add material property to sphere class to support customizable materials

--- include/raytracing/hittable.h
+++ include/raytracing/hittable.h
@@ -3,12 +3,16 @@
 
 #include "raytracing/interval.h"
 #include "raytracing/ray.h"
+#include "raytracing/rtweekend.h"
 #include "raytracing/vec3.h"
+
+class material;
 
 class hit_record {
 public:
   point3 p;
   vec3 normal;
+  shared_ptr<material> mat;
   double t;
   bool front_face;
 
--- include/raytracing/sphere.h
+++ include/raytracing/sphere.h
@@ -4,13 +4,16 @@
 #include "raytracing/hittable.h"
 #include "raytracing/interval.h"
 #include "raytracing/ray.h"
+#include "raytracing/rtweekend.h"
 #include "raytracing/vec3.h"
 #include <cmath>
 
 class sphere : public hittable {
 public:
   sphere(const point3 &center, double radius)
-      : center(center), radius(std::fmax(0, radius)) {}
+      : center(center), radius(std::fmax(0, radius)) {
+    // TODO: Initialize the material pointer `mat`.
+  }
 
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
     vec3 oc = center - r.origin();
@@ -36,6 +39,7 @@ public:
     rec.p = r.at(rec.t);
     vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
+    rec.mat = mat;
 
     return true;
   }
@@ -43,6 +47,7 @@ public:
 private:
   point3 center;
   double radius;
+  shared_ptr<material> mat;
 };
 
 #endif
