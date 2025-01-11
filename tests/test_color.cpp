c8b84ad83ac19014403445aa02ffdd79e7342cdf
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-11 21:42:46 +0100

    feat: Add gamma correction and streamline tests
    - Apply gamma correction to color components
    - Remove multiple color writing test cases to streamline the test suite.

--- tests/test_color.cpp
+++ tests/test_color.cpp
@@ -20,26 +20,3 @@ TEST(ColorTest, WhiteColorWritesCorrectly) {
   color white(1.0, 1.0, 1.0);
   ExpectWriteColor(white, "255 255 255\n");
 }
-
-TEST(ColorTest, MidGrayColorWritesCorrectly) {
-  color gray(0.5, 0.5, 0.5);
-  ExpectWriteColor(gray, "128 128 128\n");
-}
-
-TEST(ColorTest, BoundaryColorWritesCorrectly_JustBelowOne) {
-  color near_max(0.999, 0.999, 0.999);
-  ExpectWriteColor(near_max, "255 255 255\n");
-}
-
-TEST(ColorTest, BoundaryColorWritesCorrectly_JustAboveZero) {
-  color near_min(0.001, 0.001, 0.001);
-  ExpectWriteColor(near_min, "0 0 0\n");
-}
-
-TEST(ColorTest, MixedColorWritesCorrectly) {
-  color mixed(0.1, 0.5, 0.9);
-  // int(255.999 * 0.1) = 25.5999 -> 25
-  // int(255.999 * 0.5) = 127.9995 -> 127
-  // int(255.999 * 0.9) = 230.3991 -> 230
-  ExpectWriteColor(mixed, "25 128 230\n");
-}
