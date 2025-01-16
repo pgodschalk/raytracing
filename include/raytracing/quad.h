d69f13e865bc900a22a1f32ff4e5ca0ce573c5b0
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-16 15:40:35 +0100

    feat: Add quad intersection functionality
    - Implement quad intersection functionality by calculating normals, setting up bounding boxes, and enabling accurate hit detection.

--- include/raytracing/quad.h
+++ include/raytracing/quad.h
@@ -7,11 +7,16 @@
 #include "raytracing/ray.h"
 #include "raytracing/rtweekend.h"
 #include "raytracing/vec3.h"
+#include <cmath>
 
 class quad : public hittable {
 public:
   quad(const point3 &Q, const vec3 &u, const vec3 &v, shared_ptr<material> mat)
       : Q(Q), u(u), v(v), mat(mat) {
+    auto n = cross(u, v);
+    normal = unit_vector(n);
+    D = dot(normal, Q);
+
     set_bounding_box();
   }
 
@@ -25,7 +30,25 @@ public:
   aabb bounding_box() const override { return bbox; }
 
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
-    return false; // To be implemented
+    auto denom = dot(normal, r.direction());
+
+    // No hit if the ray is parallel to the plane.
+    if (std::fabs(denom) < 1e-8)
+      return false;
+
+    // Return false if the hit point parameter t is outside the ray interval.
+    auto t = (D - dot(normal, r.origin())) / denom;
+    if (!ray_t.contains(t))
+      return false;
+
+    auto intersection = r.at(t);
+
+    rec.t = t;
+    rec.p = intersection;
+    rec.mat = mat;
+    rec.set_face_normal(r, normal);
+
+    return true;
   }
 
 private:
@@ -33,6 +56,8 @@ private:
   vec3 u, v;
   shared_ptr<material> mat;
   aabb bbox;
+  vec3 normal;
+  double D;
 };
 
 #endif
