8577ba93ef488ce7fb524534559f9091c5c13a8b
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-17 11:36:06 +0100

    feat: add translation and operator support to raytracer
    - Add operators to enable offsetting an aabb with a vec3
    - Implement translation functionality for hittable objects in the ray tracer
    - Add operator overloads to enable adding a double displacement to intervals from both sides

--- include/raytracing/aabb.h
+++ include/raytracing/aabb.h
@@ -101,4 +101,12 @@ inline const aabb aabb::empty =
 inline const aabb aabb::universe =
     aabb(interval::universe, interval::universe, interval::universe);
 
+inline aabb operator+(const aabb &bbox, const vec3 &offset) {
+  return aabb(bbox.x + offset.x(), bbox.y + offset.y(), bbox.z + offset.z());
+}
+
+inline aabb operator+(const vec3 &offset, const aabb &bbox) {
+  return bbox + offset;
+}
+
 #endif
